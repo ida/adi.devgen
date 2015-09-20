86987994294be9459ccc7c0daf6a270a87a8e407
Ida Ebkes <contact@ida-ebkes.eu>
2015-09-20 16:32:07 +0200

    Add INSTALL and USAGE to long-description.

--- adi/devgen/scripts/create.py
+++ adi/devgen/scripts/create.py
@@ -31,7 +31,10 @@ version = \'0.1.dev0\'\n\
 setup(name=\'' + addon_name + '\',\n\
       version=version,\n\
       description="",\n\
-      long_description="",\n\
+      long_description=open("README.md").read() + "\n" +\n\
+                             open(os.path.join("docs", "INSTALL.md")).read(),\n\
+                             open(os.path.join("docs", "USAGE.md")).read(),\n\
+                             ",\n\
       classifiers=[\n\
         "Framework :: Plone",\n\
         "Programming Language :: Python",\n\

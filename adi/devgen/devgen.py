72ea757e8ac92763e27cebdd107fd3b2ea2a0e84
Ida Ebkes <contact@ida-ebkes.eu>
2015-09-28 19:30:35 +0200

    up

--- adi/devgen/devgen.py
+++ adi/devgen/devgen.py
@@ -52,13 +52,10 @@ def devgen():
         if not fun.startswith('__'):
             available_functions.append(fun)
 
-    #
     # HELP
-    #
-
     # No argument was provided of user:
     if len(args) < 1:
-        exit("\nAvailable functions: " + ", ".join(available_functions) + ".\n\n\
+        exit("\nAvailable functions:\n" + ", ".join(available_functions) + ".\n\n\
 Type `devgen [FUNCTION_NAME]` to see which arguments a function expects and its docstr.\n\n\
 Type `devgen help` to get a verbose description of this tool.\n")
 

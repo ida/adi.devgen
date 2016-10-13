91b7c884f9d518ace0d96b90dff58eb84f47293a
Ida Ebkes <ebkes@gw20e.com>
2016-10-13 17:20:43 +0200

    up

--- adi/devgen/helpers/users.py
+++ adi/devgen/helpers/users.py
@@ -1,3 +1,5 @@
+from Products.CMFCore.utils import getToolByName
+
 def addUser(context, user_email):
     """
     Use prefix of email as user_id, user_name and passwd.
@@ -14,5 +16,23 @@ def addUser(context, user_email):
         pt = context.portal_registration
         pt.addMember(user_id, passwd, properties=properties)
 
+def getUser(context, user_id):
+    """
+    Get user-obj by user-id, requires holding the Manager-role.
+    """
+    user = None
+    member_tool = getToolByName(context, 'portal_membership')
+    user = member_tool.getMemberById(user_id)
+    return user
+
+def setUserFullname(context, user, user_fullname):
+    """
+    Set a user's fullname.
+    """
+    user.fullname = user_fullname
+    if user.getProperty('fullname') != user_fullname:
+        user.setMemberProperties(mapping={"fullname":user_fullname})
+
 def userExists(context, user_id):
     return context.portal_membership.getMemberById(user_id) is not None
+

1599e915fc659b66de06a5f9352cf74b694fac3d
Ida Ebkes <contact@ida-ebkes.eu>
2017-10-28 07:59:47 +0200

    Add getContextOfRequest()

--- adi/devgen/helpers/misc.py
+++ adi/devgen/helpers/misc.py
@@ -1,5 +1,12 @@
 from zope.globalrequest import getRequest
 
+def getContextOfRequest(request):
+    published = request.get('PUBLISHED', None)
+    context = getattr(published, '__parent__', None)
+    if context is None:
+        context = request.PARENTS[0]
+    return context
+
 def getRequest():
     return getRequest()
 
